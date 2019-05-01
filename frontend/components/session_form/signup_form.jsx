89da3c5420f1d86a198a1aa483790eb2f351589d
aliglaser <nurinuriprettynuri@gmail.com>
2019-05-01 16:39:29 -0700

    before heroku

---
+++ frontend/components/session_form/signup_form.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+class SignupForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            username: '',
+            location: '',
+            password: ''
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    update(field) {
+        return e => this.setState({
+            [field]: e.currentTarget.value
+        });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const user = Object.assign({}, this.state);
+        this.props.processForm(user);
+    }
+
+    renderErrors() {
+        return (
+            <ul>
+                {this.props.errors.map((error, i) => (
+                    <li key={`error-${i}`}>
+                        {error}
+                    </li>
+                ))}
+            </ul>
+        );
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    Welcome to Sound Cloud! <br />
+                    Please {this.props.formType} or {this.props.navLink}
+                    {this.renderErrors()}
+                    <div>
+                        <br />
+                        <label>Email:
+              <input type="text"
+                                value={this.state.email}
+                                onChange={this.update('email')}
+                            />
+                        </label>
+                        <label>Username:
+                        <input type="text"
+                                value={this.state.username}
+                                onChange={this.update('username')}
+                            />
+                        </label>
+                        <br />
+                        <label>Password:
+              <input type="password"
+                                value={this.state.password}
+                                onChange={this.update('password')}
+                                className="login-input"
+                            />
+                        </label>
+                        <br />
+                        <input className="session-submit" type="submit" value={this.props.formType} />
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default withRouter(SignupForm);
